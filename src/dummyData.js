80fcfca95967a60b3c4642616bbab966541cfc31
Jaxon Ray Petersen <thatCashBar@gmail.com>
2022-06-19 14:11:45 -0400

    Added dummyData and changed picture formats

---
+++ src/dummyData.js
@@ -0,0 +1,52 @@
+export const Users = [
+  {
+    id: 1,
+    profilePicture: "assets/person/profilePic_1.jpg",
+    username: "Cleo Williams",
+  },
+  {
+    id: 2,
+    profilePicture: "assets/person/profilePic_2.jpeg",
+    username: "Jennifer Patterson",
+  },
+  {
+    id: 3,
+    profilePicture: "assets/person/profilePic_3.png",
+    username: "John Smith",
+  },
+  {
+    id: 4,
+    profilePicture: "assets/person/profilePic_4.png",
+    username: "Brad Thompson",
+  },
+  {
+    id: 5,
+    profilePicture: "assets/person/profilePic_5.jpg",
+    username: "Tanner Warren",
+  },
+  {
+    id: 6,
+    profilePicture: "assets/person/profilePic_6.jpg",
+    username: "Ahmed Nadar",
+  },
+  {
+    id: 7,
+    profilePicture: "assets/person/profilePic_7.jpg",
+    username: "Michael Upton",
+  },
+  {
+    id: 8,
+    profilePicture: "assets/person/profilePic_8.jpg",
+    username: "Cameron Fischer",
+  },
+  {
+    id: 9,
+    profilePicture: "assets/person/profilePic_9.jpg",
+    username: "Angelina Leonard",
+  },
+  {
+    id: 10,
+    profilePicture: "assets/person/profilePic_10.jpg",
+    username: "Katrina Evans",
+  },
+];
